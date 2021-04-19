708f3045dd359aacf746091dd5edf05d60d1b984
G4BR1EL0 <gabrielabarca.91@gmail.com>
2021-04-19 20:47:16 +0200

    incluir carrousel

---
+++ src/Components/ImageRoutes/ImageRoutes.js
@@ -0,0 +1,18 @@
+export const ImageRoutes = [
+    {
+      image:
+        'https://clinicaqualitas.com/wp-content/uploads/2019/10/home-1.jpg'
+    },
+    {
+      image:
+        'https://www.micaleclinicadental.es/wp-content/uploads/2018/08/caries-1024x681.jpg'
+    },
+    {
+      image:
+        'https://clinicadentalalpe.com/wp-content/uploads/2020/09/Tu-dentista-de-confianza-en-Madrid-Retiro-1400x630.jpg'
+    },
+    {
+      image:
+        'https://s03.s3c.es/imag/_v0/770x420/a/3/c/600x400_dentista-dreams.jpg'
+    }    
+  ];
