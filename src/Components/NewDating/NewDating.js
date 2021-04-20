a9bea2a5a7e7e6b8dd5c563d64d1f8327bbe0030
Galactuuus <galactuuus@gmail.com>
2021-04-20 23:09:35 +0200

    Error minoritario

--- src/Components/NewDating/NewDating.js
+++ src/Components/NewDating/NewDating.js
@@ -13,34 +13,19 @@ class NewDating extends Component {
       error:"",
       date:"",
       userID:"",
-<<<<<<< HEAD
       doctorArray:[],
       status: "Programada",
       detail: "",
       doctorID: 0
-=======
-      doctorID:"",
-      status: "Programado",
-      detail: "",
-      doctores:[]
->>>>>>> af8c3812412abeddb65963590e36ca73979c4f23
     }
-    this.handleSelect = this.handleSelect.bind(this);
 
   }
 
   async componentDidMount() {
     let docArray = [];   
     docArray = await ApiConsumer.listDoctors();
-<<<<<<< HEAD
     this.setState({ doctorArray: docArray}); 
   }
-=======
-    console.log(docArray)
-    this.setState({ doctores: docArray});  
-  }  
-  
->>>>>>> af8c3812412abeddb65963590e36ca73979c4f23
   handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -51,12 +36,8 @@ class NewDating extends Component {
       if (response.error){
           this.setState({ error: response.error });  
       }else{
-<<<<<<< HEAD
           this.setState({ error: null });
           this.props.history.push('/');
-=======
-          this.setState({ error: response.res });
->>>>>>> af8c3812412abeddb65963590e36ca73979c4f23
       }
     } catch(e) {
         console.log(e);
@@ -71,17 +52,9 @@ class NewDating extends Component {
   handleChange = (e) => {
     this.setState({[e.target.name]: e.target.value});
   };
-<<<<<<< HEAD
-  handlePuto = (e) => {
+  handleDoctor = (e) => {
     this.setState({ doctorID: e.target.value});
   }
-=======
-
-  handleSelect = (e) => {
-    this.setState({doctorID: e.target.value});
-    console.log(this.state.doctorID);
-  };
->>>>>>> af8c3812412abeddb65963590e36ca73979c4f23
   
   render() {
     return(
@@ -108,40 +81,13 @@ class NewDating extends Component {
             <label>
                 Doctores:
                 <br/>
-<<<<<<< HEAD
-                <select name="doctorID" onChange={this.handlePuto} id="doctors">
+                <select name="doctorID" onChange={this.handleDoctor} id="doctors">
                   {
                     this.state.doctorArray.map(doc =>  <option value={doc.id} >{doc.name + ' ' + doc.lastname}</option> )
                   }
                 </select>
             </label>
             <br/>
-=======
-                <select value={this.state.doctorID} name="doctorID" onChange={this.handleSelect} >
-                  {this.state.doctores.map((doc, index)=>{
-                    return(
-                      <option key={index} value={doc.id}>{doc.name}</option>
-                    )
-                  })}
-                </select >
-
-            </label>
-            <br/>
-            <label>
-                Status:
-                <br/>
-                <input type="text" value="Programado"
-                  name="status" enable="false" readOnly={true}/>
-            </label>
-            <br/>
-            <label>
-              Detail:
-                <br/>
-                <input type="text" value={this.state.detail}
-                  name="detail" onChange={this.handleChange}/>
-            </label>
-            <br/><br/>
->>>>>>> af8c3812412abeddb65963590e36ca73979c4f23
             <input id="submit" type="submit" value="Enviar"/>
           </form>
           {this.state.error  && <Error msg={this.state.error}/>}
